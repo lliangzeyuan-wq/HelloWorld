29f97d18b9ba9a21cbef8479c5be0906c915fb42
lliangzeyuan-wq <lliangzeyuan@gmail.com>
2026-01-16 23:35:22 +0800

    优化冒泡排序代码

--- main.cpp
+++ main.cpp
@@ -2,15 +2,19 @@
 using namespace std;
 void sort(int arr[], int size) {
 	int tmp=0;
+	bool flag=false;
 	for(int i=0;i<size-1;i++)
 	{
 		for(int j=0;j<size-1-i;j++){
 			if(arr[j]>arr[j+1]){
-				tmp=arr[j];
-				arr[j]=arr[j+1];
-				arr[j+1]=tmp;
+			tmp=arr[j];
+			arr[j]=arr[j+1];
+			arr[j+1]=tmp;
 			}
 		}
+		if(!flag){
+			break;
+		} 
 	}
 
 }
