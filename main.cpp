da85eca596e86a8902e2461751ed1521d829d8fb
lliangzeyuan-wq <lliangzeyuan@gmail.com>
2026-01-17 00:46:46 +0800

    修改算法为快排分割算法

--- main.cpp
+++ main.cpp
@@ -1,27 +1,37 @@
-#inlucr<iostream>
+#include<iostream>
 using namespace std;
-void sort(int arr[], int size) {
-	int tmp=0;
-	bool flag=false;
-	for(int i=0;i<size-1;i++)
-	{
-		for(int j=0;j<size-1-i;j++){
-			if(arr[j]>arr[j+1]){
-			tmp=arr[j];
-			arr[j]=arr[j+1];
-			arr[j+1]=tmp;
-			}
-		}
-		if(!flag){
-			break;
-		} 
+void sort(int arr[], int i, int j) {
+	if (i >= j) {
+		return;
 	}
-
+	int val = arr[i];
+	int l = i;
+	int r = j;
+	while (l < r) {
+		while (l < r && arr[r] >= val) {
+			r--;
+		}
+		if (l < r) {
+			arr[l++] = arr[r];
+		}
+		while (l < r && arr[l] < val) {
+			l++;
+		}
+		if (l < r) {
+			arr[r--] = arr[l];
+		}
+	}
+	arr[l] = val;
+	sort(arr, i, l - 1);
+	sort(arr, l + 1, j);
 }
 int main() {
 	int arr[] = { 12,4,89,43,21,78 };
 	int size = sizeof(arr) / sizeof(arr[0]);
-	sort(arr, size);
+	sort(arr, 0, size - 1);
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << endl;
+	}
 	return 0;
 }
 //github添加的注释
