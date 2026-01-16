9ef71af500a332279d9a71ec89b027ac779a8217
lzy <lliangzeyuan@qq.com>
2026-01-16 23:21:50 +0800

    小张修改sort函数从大到小排序

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ void sort(int arr[], int size) {
 	for(int i=0;i<size-1;i++)
 	{
 		for(int j=0;j<size-1-i;j++){
-			if(arr[j]>arr[j+1]){
+			//小张修改排序算法，从大到小
+			if(arr[j]<arr[j+1]){
 				tmp=arr[j];
 				arr[j]=arr[j+1];
 				arr[j+1]=tmp;
